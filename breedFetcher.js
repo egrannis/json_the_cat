5f7dcd935a735e7850171eb033165bb3442cb573
Emma Grannis <grannis.emma@gmail.com>
2022-04-14 20:39:13 +0000

    Fixed mistake caught in mocha testing

--- breedFetcher.js
+++ breedFetcher.js
@@ -1,20 +1,20 @@
 const request = require('request');
 const args = process.argv.slice(2);
 const breedName = args[0];
-let URL = `https://qqapi.thecatapi.com/v1/breeds/search?q=${breedName}`;
+let URL = `https://api.thecatapi.com/v1/breeds/search?q=${breedName}`;
 
-
-const fetchBreedDescription = function(breedName, callback) {// callback takes 2 parameters, err + descrip
+const fetchBreedDescription = function(breedName, callback) {
   request(URL, (error, response, body) => {
-    if (body === '[]') { // haven't parsed through the data because
-      return callback(`Error: breed not found`, null);
-    }
     if (error) {
       return callback(error, null); //error, which means we wouldn't get a desctiption, so descrip would be null
     }
     const data = JSON.parse(body); // need to manipulate body for query to come into play
     const breed = data[0];
-    callback(null, breed.description);
+    if (breed) { // if breed exists, we want to find the description, if breed not found we want to say breed not found
+      return callback(null, breed.description);
+    } else {
+      return callback(`Failed to find breed: ${breedName}`);
+    }
   });
 };
 
@@ -23,16 +23,16 @@ module.exports = { fetchBreedDescription };
 // Below is an alternative way of writing the function
 // const fetchBreedDescription = function(breedName, callback) {// callback takes 2 parameters, err + descrip
 
+// const fetchBreedDescription = function(breedName, callback) {// callback takes 2 parameters, err + descrip
 //   request(URL, (error, response, body) => {
+//     if (body === '[]') { // haven't parsed through the data because
+//       return callback(`Error: breed not found`, null);
+//     }
 //     if (error) {
 //       return callback(error, null); //error, which means we wouldn't get a desctiption, so descrip would be null
 //     }
 //     const data = JSON.parse(body); // need to manipulate body for query to come into play
 //     const breed = data[0];
-//     if (breed) { // if breed exists, we want to find the description, if breed not found we want to say breed not found
-//       return callback(null, breed.description);
-//     } else {
-//       return callback(`Failed to find breed: ${breedName}`)
-//     }
-//   })
+//     callback(null, breed.description);
+//   });
 // };
