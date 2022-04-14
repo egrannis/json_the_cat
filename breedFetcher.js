671026a707b2c47cda8c7ef71e0da34ec1dff644
Emma Grannis <grannis.emma@gmail.com>
2022-04-14 19:14:50 +0000

    Implemented edge cases

--- breedFetcher.js
+++ breedFetcher.js
@@ -1,12 +1,21 @@
 const request = require('request');
 const args = process.argv.slice(2);
 const breedName = args[0];
+let URL = `https://api.jsbgurbgibrthecatapi.com/v1/breeds/search?q=${breedName}` ;
 
-request(`https://api.thecatapi.com/v1/breeds/search?q=${breedName}`, (error, response, body) => {
+request(URL, (error, response, body) => {
   if (error) {
-  console.log('Error: breed not found', error); // Print the error if one occurred
+  console.log('Error: URL not valid'); // Print the error if one occurred
   return;
   }
-  const data = JSON.parse(body);
-  console.log(data[0].description); 
+  const data = JSON.parse(body); // need to manipulate body for query to come into play
+  const breed = data[0];
+  if (breed) {
+  console.log(breed.description); 
+  } else {
+  console.log(`Failed to find breed: ${breedName}`);
+  }
 });
+
+
+// if breed exists, we want to find the description, if breed not found we want to say breed not found
